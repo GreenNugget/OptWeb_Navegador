dcb5eb3a79c82552e2bd354e184348bc77fd77eb
GreenNugget <naomi.snchz99@gmail.com>
2020-03-04 10:10:31 -0600

    Funciona

--- rss.php
+++ rss.php
@@ -17,7 +17,7 @@ foreach ($feed->get_items($items, $items) as $item) {
     echo '<div class="container">';
     echo '<p class="title">Título: <a href="' . $item->get_link() . '">' . $item->get_title() . '</a></p>';
     echo '<p class="newsB">Autor: ' . $item->get_author()->get_name() . '</p>';
-    echo '<p class="newsB">Fecha: ' . $item->get_date('Y-m-d H:i:s') . '</p>';
+    echo '<p class="newsB">Fecha: ' . $item->get_date('Y-m-d') . '</p>';
     echo '<p class="newsB">Descripción: ' . $item->get_description() . '</p>';
     echo '</div>';
     echo '</header>';
