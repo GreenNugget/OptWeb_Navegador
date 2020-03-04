ca4a351047e32d0c7e0cb47598fadef649ea991c
GreenNugget <naomi.snchz99@gmail.com>
2020-03-04 15:29:58 -0600

    Ya está conectada la base de datos y ya almacena los datos

--- rss.php
+++ rss.php
@@ -8,21 +8,31 @@ $feed->set_feed_url($url);
 $feed->enable_cache();
 $feed->init();
 
-echo '<div class="main-title" align="middle">TUS NOTICIAS';
-echo '</div>';
-foreach ($feed->get_items($items, $items) as $item) {
-    echo '<link rel="stylesheet" href="style.css">';
-    echo '<article>';
-    echo '<header>';
-    echo '<div class="container">';
-    echo '<p class="title">Título: <a href="' . $item->get_link() . '">' . $item->get_title() . '</a></p>';
-    echo '<p class="newsB">Autor: ' . $item->get_author()->get_name() . '</p>';
-    echo '<p class="newsB">Fecha: ' . $item->get_date('Y-m-d') . '</p>';
-    echo '<p class="newsB">Descripción: ' . $item->get_description() . '</p>';
-    echo '</div>';
-    echo '</header>';
-    echo $item->get_content(true);
-    echo '</article>';
+//Se establece la conexión con la base de datos
+$conexion = mysqli_connect('localhost','root','','rss_news');
+
+if($conexion){    
+    $link;
+    $titulo;
+    $autor;
+    $fecha;
+    $descripcion;
+    
+    foreach ($feed->get_items($items, $items) as $item) {
+        $link = $item->get_link();
+        $titulo = $item->get_title();
+        $autor = $item->get_author()->get_name();
+        $fecha = $item->get_date('Y-m-d');
+        $descripcion = $item->get_description();
+        echo $item->get_content(true);
+
+        $sql = "insert into `noticias` (link, titulo, autor, fecha, descripcion) values ('$link', '$titulo', '$autor', $fecha,'$descripcion')";
+        echo $sql;
+        $resultado = $conexion->query($sql);
+    }
+    $conexion->close();
+    echo '<button class="back-button"> <a href="../RSS(Proyecto)/index.html">Haz clic aquí para seleccionar otra noticia :o</a> </button>';
+}else{
+    echo'alert("No se pudo conectar a la base de datos");';
 }
-echo '<button class="back-button"> <a href="../RSS(Proyecto)/index.html">Haz clic aquí para seleccionar otra noticia :o</a> </button>';
 ?>
