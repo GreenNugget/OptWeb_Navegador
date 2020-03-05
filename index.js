b85c7e7304d549c2ea263a61b6eb211911bb779e
GreenNugget <naomi.snchz99@gmail.com>
2020-03-05 08:47:47 -0600

    Ya se tiene el algoritmo de ordenamiento

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 window.onload = function(){
     document.getElementById('verNoticias').onclick = function(){
-        //var dates = [];
+        //var dates = <?php echo json_encode($arregloFechas); ?>;
     };
 }
 
-function sortBy(){
+/* Algoritmo de ordenamiento que recibe como parámetro un array */
+function quickSort(array) {
+    if (array.length < 1) {
+        return [];
+    }
+    var left = [];
+    var right = [];
+    var pivot = array[0];
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] < pivot) {
+            left.push(array[i]);
+        }
+        else {
+            right.push(array[i]);
+        }
+    }
 
+    return [].concat(quickSort(left), pivot, quickSort(right));
 }
