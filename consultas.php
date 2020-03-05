81e8a13ceba8ad9a70c1cc53b40bf7d561a01854
GreenNugget <naomi.snchz99@gmail.com>
2020-03-05 09:13:04 -0600

    Se está implementando la parte de obtener las fechas

--- consultas.php
+++ consultas.php
@@ -3,9 +3,20 @@
 $conexion = mysqli_connect('localhost', 'root', '', 'rss_news');
 
 if($conexion){
-    mysqli_select_db($conexion,'rss_news'){
-        
+    $arregloFechas = [];
+    mysqli_select_db($conexion,'rss_news');
+    $result = mysqli_query($conexion,"select 'fecha' from noticias");
+    while(($fila = mysqli_fetch_array($result))!=NULL){
+        //echo $result;
+        $fila = $result -> fetch_array(MYSQLI_ASSOC);
+        array_push($arregloFechas,$fila);
     }
+    echo "<script>
+        var dates = <?php echo json_encode($arregloFechas);?>;
+        for(var i=0;i<dates.length;i++){
+            document.write(dates(i));
+        }
+    </script>";
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <head>
     <title>Noticias</title>
     <meta charset="UTF-8">
@@ -6,17 +7,18 @@
     <script src="index.js"></script>
     <link rel="stylesheet" href="bootstrap-4.3.1-dist/css/bootstrap.min.css">
 </head>
+
 <body>
     <section class="container">
         <form action="rss.php" method="POST" class="text-center">
             Url de noticias:
-            <input type="text" id="url" name="url" placeholder="Ingresa tu url aquí :D"/><br><br>
-            Número de noticias que quieres ver: 
-            <input type="text" id="numN" name="numN" required/><br><br>
+            <input type="text" id="url" name="url" placeholder="Ingresa tu url aquí :D" /><br><br>
+            Número de noticias que quieres ver:
+            <input type="text" id="numN" name="numN" required /><br><br>
             <input type="submit" id="verNoticias" value="Ver Noticias">
         </form>
     </section>
-    <?php include 'consultas.php';?>
+    <?php include 'consultas.php'; ?>
     <div id="news_Space">
     </div>
 </body>
