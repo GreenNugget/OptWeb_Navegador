0702cc98c4d41ce8232396fff42046a15883025c
GreenNugget <naomi.snchz99@gmail.com>
2020-03-09 13:48:10 -0600

    Se le agregaron estilos al index

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body class="bg-light">
     <div class="container">
         <header class="page-header">
-            <i> <img src="https://upload.wikimedia.org/wikipedia/commons/4/43/Feed-icon.svg" alt="rss" width="30px" height="30px"> </i>
+            <i> <img src="https://upload.wikimedia.org/wikipedia/commons/4/43/Feed-icon.svg" alt="rss_logo" width="30px" height="30px"> </i>
             <h3>Lector de noticias</h3>
         </header> <br>
         <form action="rss.php" method="POST" class="text-center">
@@ -27,6 +27,15 @@
         </form> <br>
     </div>
     <?php include 'consultas.php'; ?>
-    <div id="news_Space">
+    <div class="news_Space" id="news_Space">
+        <div class="row2">
+            <label for="select_sort">Elija una forma de ordenamiento: </label>
+            <select class="select_sort" name="select_sort" id="select_sort">
+                <option value="day">Día</option>
+                <option value="month">Mes</option>
+                <option value="year">Año</option>
+            </select>
+            <button class="btn btn-warning" id="sort_button">Ordenar</button>
+        </div>
     </div>
 </body>
