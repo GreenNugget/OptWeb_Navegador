f3421db886f942781fbd514ce6fe1efba644dfb7
GreenNugget <naomi.snchz99@gmail.com>
2020-03-09 14:27:05 -0600

    Se agregó la descripción de la info, falta mandar datos por fecha

--- consultas.php
+++ consultas.php
@@ -8,32 +8,16 @@ if($conexion){
     mysqli_select_db($conexion, "noticias");//se selecciona la bd de donde vamos a obtener datos
 
     $resultado = mysqli_query($conexion,$consulta, MYSQLI_STORE_RESULT);
-
-    echo "<div class='row'>";
+    
     while ($fila = mysqli_fetch_array($resultado, MYSQLI_NUM)) {
         echo "<p>
         Link: <a href='$fila[0]'  target='_blank'>$fila[0]</a><br>
-        $fila[1];<br>
-        $fila[2];<br>
-        $fila[3];<br>
-        $fila[4];<br>
+        Título: $fila[1]<br>
+        Autor: $fila[2]<br>
+        Fecha: $fila[3]<br>
+        Descripción: $fila[4]<br>
         </p>";
     }
-    echo "</div>";
-
-   /* $arregloFechas = [];
-    mysqli_select_db($conexion,'rss_news');
-    $result = mysqli_query($conexion,"select 'fecha' from noticias");
-    while(($fila = mysqli_fetch_array($result))!=NULL){
-        $fila = $result -> fetch_array(MYSQLI_ASSOC);
-        array_push($arregloFechas,$fila);
-    }
-    echo "<script>
-        var dates = <?php echo json_encode($arregloFechas);?>;
-        for(var i=0;i<dates.length;i++){
-            document.write(dates(i));
-        }
-    </script>";*/
 }
 
 ?>
