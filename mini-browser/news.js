2ab0833658c6e5014ce4d3e14f563eb584ebc7bf
GreenNugget <naomi.snchz99@gmail.com>
2020-04-16 03:11:53 -0500

    Haciendo pruebas con la bd

---
+++ mini-browser/news.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+
+    document.getElementById('watchAll').onclick = function () {
+        document.getElementById('newsContainer').className = 'container my-5 bg-dark text-white d-block';
+    }
+
+
+
+
+}
